6e29e3ec627dde5906365ac6eafd1ea470171146
Jay Takahashi <qsakura14555@gmail.com>
2023-11-02 22:53:44 +0900

    Rename gnl

--- README.md
+++
@@ -1,2 +0,0 @@
-# libft
-The aim of this project is to code a C library regrouping usual functions that we’ll be allowed to use in all our other projects. 
--- src/gnl/get_next_line.c
+++ src/gnl/get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include "get_next_line.h"
 
 static char	*new_file_str(char *file_str)
 {
