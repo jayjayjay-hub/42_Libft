55082b1bf7011c5aed32a23c1282bdfce9b38b1f
Jay Takahashi <qsakura14555@gmail.com>
2024-04-09 09:59:27 +0900

    fix bug

--- include/get_next_line.h
+++ include/get_next_line.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jtakahas <jtakahas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jay <jay@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 13:14:48 by jtakahas          #+#    #+#             */
-/*   Updated: 2023/10/09 16:16:10 by jtakahas         ###   ########.fr       */
+/*   Updated: 2024/04/09 09:51:02 by jay              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 100
-#  define OPEN_MAX 1024
 # endif
 
+# define OPEN_MAX 1024
 char	*get_next_line(int fd);
-size_t	ft_strlen(const char *str);
-char	*ft_strchr(char *str, int c);
-char	*ft_strjoin(char *str, char *buf);
+size_t	ft_strlen_gnl(const char *str);
+char	*ft_strchr_gnl(char *str, int c);
+char	*ft_strjoin_gnl(char *str, char *buf);
 #endif
--- src/gnl/get_next_line_utils.c
+++ src/gnl/get_next_line_utils.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jtakahas <jtakahas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jay <jay@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 11:46:06 by jtakahas          #+#    #+#             */
-/*   Updated: 2023/10/09 16:37:19 by jtakahas         ###   ########.fr       */
+/*   Updated: 2024/04/09 09:50:35 by jay              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-size_t	ft_strlen(const char *str)
+size_t	ft_strlen_gnl(const char *str)
 {
 	size_t	count;
 
@@ -24,7 +24,7 @@ size_t	ft_strlen(const char *str)
 	return (count);
 }
 
-char	*ft_strchr(char *str, int c)
+char	*ft_strchr_gnl(char *str, int c)
 {
 	size_t	index;
 
@@ -38,13 +38,13 @@ char	*ft_strchr(char *str, int c)
 	return (NULL);
 }
 
-char	*ft_strjoin(char *str, char *buf)
+char	*ft_strjoin_gnl(char *str, char *buf)
 {
 	size_t	i;
 	size_t	j;
 	char	*new_str;
 
-	new_str = malloc(sizeof(char) * ((ft_strlen(str) + ft_strlen(buf)) + 1));
+	new_str = malloc(sizeof(char) * ((ft_strlen_gnl(str) + ft_strlen_gnl(buf)) + 1));
 	if (!new_str)
 	{
 		free(str);
